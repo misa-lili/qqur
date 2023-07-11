f5fcd211889dd9bb02b3410187512dbd9c59a711
adt-hgkim <hgkim@gnmpm.com>
2023-07-12 02:10:29 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		nodePolyfills({
-			globals: {
-				Buffer: true, // can also be 'build', 'dev', or false
-				global: true,
-				process: true
-			},
-			protocolImports: true
-		})
-	]
+	plugins: [sveltekit()],
+	define: {
+		global: 'window'
+	}
 });
